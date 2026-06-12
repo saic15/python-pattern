a71d3fde9825758e975bb2e0a4821be02df111c9
saic15 <sp4054317@gmail.com>
2026-06-12 21:17:35 +0530

    voting eligibility

---
+++ voting eligibility.py
@@ -0,0 +1,13 @@
+n=input("Enter a name:")
+age=int(input("Enter a age:"))
+gender=input("Enter a gender M/F:")
+if (age>=18 and age<60):
+    print("no")
+elif(g=="M" and age>30 and age,60):
+    print("yes")
+elif(g=="F" and age>18):
+    print("yes")
+else:
+    print("no")
+    
+    
